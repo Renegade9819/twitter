346ec240b185235b62aed3710f1c6a2ba32d00bc
Renegade <bhavinpbhanushali@gmail.com>
2021-12-25 21:19:35 +0530

    Profile Page update

---
+++ lib/components/CustomSliverAppBarDelegate.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class CustomSliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  final double expandedHeight;
+
+  const CustomSliverAppBarDelegate({required this.expandedHeight});
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    double size = 40 - shrinkOffset * 0.1;
+    double top = expandedHeight - shrinkOffset - size;
+    return Stack(
+      fit: StackFit.expand,
+      clipBehavior: Clip.none,
+      children: [
+        buildBackground(shrinkOffset),
+        buildAppBar(shrinkOffset),
+        Positioned(
+          top: top,
+          left: 15,
+          child: buildAvatar(shrinkOffset),
+        )
+      ],
+    );
+  }
+
+  @override
+  double get maxExtent => expandedHeight;
+
+  @override
+  double get minExtent => kToolbarHeight + 30;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      true;
+
+  Widget buildAppBar(double shrinkOffset) {
+    return Opacity(
+      opacity: appear(shrinkOffset),
+      child: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios_new),
+          onPressed: () {},
+        ),
+        title: const Text('Profile'),
+      ),
+    );
+  }
+
+  double appear(double shrinkOffset) {
+    return shrinkOffset / expandedHeight;
+  }
+
+  double disappear(double shrinkOffset) {
+    return 1 - shrinkOffset / expandedHeight;
+  }
+
+  Widget buildBackground(double shrinkOffset) {
+    return Opacity(
+      opacity: disappear(shrinkOffset),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.network(
+            "https://images.unsplash.com/photo-1640100921996-8798b85703c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=894&q=80",
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            top: 10,
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.arrow_back_ios_new),
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildAvatar(double shrinkOffset) {
+    return Opacity(
+      opacity: disappear(shrinkOffset),
+      child: CircleAvatar(
+        minRadius: 40 - shrinkOffset * 0.1,
+        backgroundColor: Colors.white,
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          minRadius: 39 - shrinkOffset * 0.1,
+          backgroundImage: const NetworkImage(
+              "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png"),
+        ),
+      ),
+    );
+  }
+}
--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:twitter/components/CustomSliverAppBarDelegate.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -20,108 +21,89 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ),
           SliverList(
               delegate: SliverChildListDelegate([
-            Container(
-              height: 1000,
-              color: Colors.blueGrey,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: const EdgeInsets.fromLTRB(20, 40, 0, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Name',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '@username',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Edit Profile',
+                      style: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.white),
+                        overlayColor: MaterialStateProperty.all(Colors.black26),
+                        side: MaterialStateProperty.all(
+                          BorderSide(style: BorderStyle.solid),
+                        ),
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)))),
+                  ),
+                ),
+              ],
             ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(20, 10, 0, 0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.bubble_chart,
+                        color: Colors.black54,
+                      ),
+                      const SizedBox(width: 5),
+                      Text(
+                        'Born September 2, 1998',
+                        style: TextStyle(fontWeight: FontWeight.w300),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.calendar_today,
+                        color: Colors.black54,
+                      ),
+                      const SizedBox(width: 5),
+                      Text(
+                        'Joined December 2021',
+                        style: TextStyle(fontWeight: FontWeight.w300),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
           ]))
         ],
-      ),
-    );
-  }
-}
-
-class CustomSliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  final double expandedHeight;
-
-  const CustomSliverAppBarDelegate({required this.expandedHeight});
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    double size = 40 - shrinkOffset * 0.1;
-    double top = expandedHeight - shrinkOffset - size;
-    return Stack(
-      fit: StackFit.expand,
-      clipBehavior: Clip.none,
-      children: [
-        buildBackground(shrinkOffset),
-        buildAppBar(shrinkOffset),
-        Positioned(
-          top: top,
-          left: 25,
-          child: buildAvatar(shrinkOffset),
-        )
-      ],
-    );
-  }
-
-  @override
-  double get maxExtent => expandedHeight;
-
-  @override
-  double get minExtent => kToolbarHeight + 30;
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
-      true;
-
-  Widget buildAppBar(double shrinkOffset) {
-    return Opacity(
-      opacity: appear(shrinkOffset),
-      child: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_new),
-          onPressed: () {},
-        ),
-        title: const Text('Profile'),
-      ),
-    );
-  }
-
-  double appear(double shrinkOffset) {
-    return shrinkOffset / expandedHeight;
-  }
-
-  double disappear(double shrinkOffset) {
-    return 1 - shrinkOffset / expandedHeight;
-  }
-
-  Widget buildBackground(double shrinkOffset) {
-    return Opacity(
-      opacity: disappear(shrinkOffset),
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.network(
-            "https://images.unsplash.com/photo-1640100921996-8798b85703c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=894&q=80",
-            fit: BoxFit.cover,
-          ),
-          Positioned(
-            top: 10,
-            child: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.arrow_back_ios_new),
-              color: Colors.white,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildAvatar(double shrinkOffset) {
-    return Opacity(
-      opacity: disappear(shrinkOffset),
-      child: CircleAvatar(
-        minRadius: 40 - shrinkOffset * 0.1,
-        backgroundColor: Colors.white,
-        child: CircleAvatar(
-          backgroundColor: Colors.transparent,
-          minRadius: 39.8 - shrinkOffset * 0.1,
-          backgroundImage: const NetworkImage(
-              "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png"),
-        ),
       ),
     );
   }
